362c1297fac6b3819defcd8201390e2aad962ef9
Jean-André Santoni <jean.andre.santoni@gmail.com>
2023-03-18 10:40:35 +0700

    Initial commit

---
+++ gfx.zig
@@ -0,0 +1,27 @@
+pub const FRAMEBUFFER: *[320*240]u8 = @intToPtr(*[320*240]u8, 120);
+
+pub fn blit(spr: *const [256]u8, dx: i32, dy: i32, trans: u8, flip: bool) void {
+	var x: u32 = 0;
+	var y: u32 = 0;
+	var x2: i32 = dx;
+	var y2: i32 = dy;
+
+	while (true) {
+		var c = if (flip) spr[15-x+y*16] else spr[x+y*16];
+
+		if (c != trans and x2 >= 0 and x2 < 320 and y2 >= 0 and y2 <= 240) {
+			FRAMEBUFFER[@intCast(u32, x2+y2*320)] = c;
+		}
+
+		x += 1;
+		x2 += 1;
+
+		if (x >= 16) {
+			x = 0;
+			x2 = dx;
+			y += 1;
+			y2 += 1;
+		}
+		if (y >= 16) { break; }
+	}
+}
