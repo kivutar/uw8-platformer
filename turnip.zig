66265074e043d96a70543ef4eee7bebbd7b4abd4
Jean-André Santoni <jean.andre.santoni@gmail.com>
2023-03-18 16:48:13 +0700

    Collisions

--- turnip.zig
+++ turnip.zig
@@ -1,6 +1,8 @@
 const sprites = @import("sprites.zig");
+const map = @import("map.zig");
 const gfx = @import("gfx.zig");
 const anim = @import("anim.zig");
+const utils = @import("utils.zig");
 
 var turnip_run_anim = anim.Anim {
 	.frames = [4][256]u8{
@@ -12,87 +14,28 @@ var turnip_run_anim = anim.Anim {
 };
 
 var turnip_stand_anim = anim.Anim {
-	.frames = [][256]u8{
+	.frames = [4][256]u8{
 		sprites.turnip_spr3,
+        sprites.turnip_spr3,
+        sprites.turnip_spr3,
+        sprites.turnip_spr3,
 	},
 };
 
 var turnip_jump_anim = anim.Anim {
-	.frames = [][256]u8{
+	.frames = [4][256]u8{
 		sprites.turnip_spr0,
+        sprites.turnip_spr0,
+        sprites.turnip_spr0,
+        sprites.turnip_spr0,
 	},
 };
 
-// type turnip struct {
-// 	rigid
-// 	anim *Anim
-// 	flip uint
-// }
+extern fn isButtonPressed(btn: i32) i32;
 
-// func (self *turnip) ontheground() bool {
-// 	return level[int((self.y+self.width)/16)][int(self.x/16)] == 0x1 || level[int((self.y+16)/16)][int((self.x+self.width-1)/16)] == 0x1
-// }
-
-// func (self *turnip) update(pad uint8) {
-// 	grounded := self.ontheground()
-
-// 	if pad&w4.BUTTON_LEFT != 0 {
-// 		self.xaccel = -0.1
-// 	} else if pad&w4.BUTTON_RIGHT != 0 {
-// 		self.xaccel = 0.1
-// 	} else {
-// 		self.xaccel = 0
-// 	}
-
-// 	self.xspeed += self.xaccel
-// 	self.yspeed += self.yaccel
-
-// 	if grounded && self.yspeed >= 0 {
-// 		self.y = float64(int(self.y/16)) * 16
-// 		self.yspeed = 0
-// 		self.yaccel = 0
-// 		if pad&w4.BUTTON_1 != 0 {
-// 			w4.Tone(300, 1, 100, w4.TONE_TRIANGLE)
-// 			self.yspeed = -4
-// 		}
-// 	} else {
-// 		self.yaccel = 0.2
-// 	}
-
-// 	self.xspeed = clamp(self.xspeed, -2, 2)
-// 	self.yspeed = clamp(self.yspeed, -4, 4)
-// 	self.x += self.xspeed
-// 	self.y += self.yspeed
-// 	self.xspeed = xfriction(self.xspeed, pad)
-
-// 	if level[int((self.y)/16)][int((self.x+self.width)/16)] == 0x1 && self.xspeed > 0 {
-// 		self.x = float64(int(self.x/16))*16 + 16 - self.width
-// 		self.xspeed = 0
-// 		self.xaccel = 0
-// 	}
-
-// 	if level[int((self.y)/16)][int((self.x)/16)] == 0x1 && self.xspeed < 0 {
-// 		self.x = float64(int(self.x/16))*16 + 16
-// 		self.xspeed = 0
-// 		self.xaccel = 0
-// 	}
-
-// 	if self.xspeed > 0 {
-// 		self.flip = w4.BLIT_FLIP_X
-// 	} else if self.xspeed < 0 {
-// 		self.flip = 0
-// 	}
-
-// 	if self.yspeed != 0 {
-// 		self.anim = &turnip_jump_anim
-// 	} else if pad&w4.BUTTON_RIGHT != 0 || pad&w4.BUTTON_LEFT != 0 {
-// 		self.anim = &turnip_run_anim
-// 	} else if self.xspeed == 0 {
-// 		self.anim = &turnip_stand_anim
-// 	}
-
-// 	self.anim.counter++
-// }
+fn lvl_at(x: f32, y: f32) i32 {
+    return map.lvl[@floatToInt(usize, y)][@floatToInt(usize, x)];
+}
 
 pub const Turnip = struct {
     const Self = @This();
@@ -103,19 +46,81 @@ pub const Turnip = struct {
 	height: f32,
     anim:   *anim.Anim = undefined,
     flip:   bool = false,
-	// speed:  f32,
-	// angle:  f32,
-	// xspeed: f32,
-	// yspeed: f32,
-	// xaccel: f32,
-	// yaccel: f32,
+	xspeed: f32 = 0,
+	yspeed: f32 = 0,
+	xaccel: f32 = 0,
+	yaccel: f32 = 0,
+
+    fn ontheground(self: *Self) bool {
+        return lvl_at(self.x/16, (self.y+self.width)/16) == 1 or lvl_at((self.x+self.width-1)/16, (self.y+16)/16) == 1;
+    }
 
     pub fn update(self: *Self) void {
-        self.anim = &turnip_run_anim;
+        var grounded = self.ontheground();
+
+        if (isButtonPressed(2) != 0) {
+            self.xaccel = -0.1;
+        } else if (isButtonPressed(3) != 0) {
+            self.xaccel = 0.1;
+        } else {
+            self.xaccel = 0;
+        }
+
+        self.xspeed += self.xaccel;
+        self.yspeed += self.yaccel;
+
+        if (grounded and self.yspeed >= 0) {
+            self.y = @intToFloat(f32, (@floatToInt(i32, self.y/16)) * 16);
+            self.yspeed = 0;
+            self.yaccel = 0;
+            // if pad&w4.BUTTON_1 != 0 {
+            //     w4.Tone(300, 1, 100, w4.TONE_TRIANGLE)
+            //     self.yspeed = -4
+            // }
+        } else {
+            self.yaccel = 0.2;
+        }
+
+        self.xspeed = utils.clamp(self.xspeed, -2, 2);
+        self.yspeed = utils.clamp(self.yspeed, -4, 4);
+        self.x += self.xspeed / 20;
+        self.y += self.yspeed / 20;
+        self.xspeed = utils.xfriction(self.xspeed);
+
+        if (lvl_at((self.x+self.width)/16, self.y/16) == 1 and self.xspeed > 0) {
+            self.x = @intToFloat(f32, (@floatToInt(i32, (self.x/16))*16 + 16 - @floatToInt(i32,self.width)));
+            self.xspeed = 0;
+            self.xaccel = 0;
+        }
+
+        if (lvl_at((self.x)/16, self.y/16) == 1 and self.xspeed < 0) {
+            self.x = @intToFloat(f32, (@floatToInt(i32, (self.x/16))*16 + 16));
+            self.xspeed = 0;
+            self.xaccel = 0;
+        }
+
+        if (self.xspeed > 0) {
+            self.flip = true;
+        } else if (self.xspeed < 0) {
+            self.flip = false;
+        }
+
+        if (self.yspeed != 0) {
+            self.anim = &turnip_jump_anim;
+        } else if (isButtonPressed(2) != 0 or isButtonPressed(3) != 0) {
+            self.anim = &turnip_run_anim;
+        } else if (self.xspeed == 0) {
+            self.anim = &turnip_stand_anim;
+        }
+
         self.anim.counter += 1;
+
+        if (self.y > 240) {
+            self.y = 0;
+        }
     }
 
     pub fn draw(self: *Self) void {
-        gfx.blit(&self.anim.frames[(self.anim.counter/160)%(self.anim.frames.len)][0], @floatToInt(i32, self.x), @floatToInt(i32, self.y), 0xe8, self.flip);
+        gfx.blit(&self.anim.frames[(self.anim.counter/8)%(self.anim.frames.len)][0], @floatToInt(i32, self.x), @floatToInt(i32, self.y), 0xe8, self.flip);
     }
 };
---
+++ utils.zig
@@ -0,0 +1,42 @@
+extern fn isButtonPressed(btn: i32) i32;
+
+pub fn xfriction(v: f32) f32 {
+    var v2 = v;
+	if (v2 < 0 and isButtonPressed(2) == 0) {
+		v2 += 0.1;
+		if (v2 > 0) {
+			v2 = 0;
+		}
+	} else if (v2 > 0 and isButtonPressed(3) == 0) {
+		v2 -= 0.1;
+		if (v2 < 0) {
+			v2 = 0;
+		}
+	}
+	return v2;
+}
+
+// pub fn yfriction(v float64, pad uint8) float64 {
+// 	if v < 0 && (pad&w4.BUTTON_UP == 0) {
+// 		v += 0.1
+// 		if v > 0 {
+// 			v = 0
+// 		}
+// 	} else if v > 0 && (pad&w4.BUTTON_DOWN == 0) {
+// 		v -= 0.1
+// 		if v < 0 {
+// 			v = 0
+// 		}
+// 	}
+// 	return v
+// }
+
+pub fn clamp(v: f32, min: f32, max: f32) f32 {
+	if (v < min) {
+		return min;
+	}
+	if (v > max) {
+		return max;
+	}
+	return v;
+}
