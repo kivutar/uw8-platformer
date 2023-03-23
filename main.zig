6ef2f73f3b1c2d234d9266ca8a21ce772ad113a7
Jean-André Santoni <jean.andre.santoni@gmail.com>
2023-03-23 14:19:37 +0700

    Paralax

--- main.zig
+++ main.zig
@@ -14,7 +14,7 @@ extern fn setPixel(x: i32, y: i32, color: i32) void;
 extern fn randomSeed(i32) void;
 
 var sun1 = sun.Sun.init(32, 32);
-var tur1 = turnip.Turnip.init(32, 32);
+var tur1 = turnip.Turnip.init(64, 240 - 32 - 16);
 
 var entities = [_]*entity.Entity{
     &sun1.entity,
@@ -46,14 +46,14 @@ export fn upd() void {
 fn draw_clouds() void {
     for (0..map.lvl.len) |y| {
         for (0..map.lvl[0].len) |x| {
-            for (2..4) |s| {
+            for (1..4) |s| {
                 var fs = @intToFloat(f32, s * 8);
                 if (sky[(x * y + s) % sky.len] > y * 16 * @sizeOf(i64)) {
                     var fx = @intToFloat(f32, x);
                     var fy = @intToFloat(f32, y);
                     if (fy * 16 > cos(fx * 16 / 40) * 40 + 240 - 100 + fs) {
                         var cosize = fs + @floatCast(f32, cos(frames / 30 + fx));
-                        circle(fx * 16 - 32 + fx, fy * 16, cosize, 232);
+                        circle(fx * 16 - camera.x / 3, fy * 16, cosize, 232);
                     }
                 }
             }
@@ -62,14 +62,14 @@ fn draw_clouds() void {
 
     for (0..map.lvl.len) |y| {
         for (0..map.lvl[0].len) |x| {
-            for (2..4) |s| {
+            for (1..4) |s| {
                 var fs = @intToFloat(f32, s * 8);
                 if (sky[(x * y + s - 100) % sky.len] > y * 16 * @sizeOf(i64)) {
                     var fx = @intToFloat(f32, x);
                     var fy = @intToFloat(f32, y);
                     if (fy * 16 > cos(fx * 16 / 40) * 40 + 240 - 100 + fs) {
                         var cosize = fs + @floatCast(f32, cos(frames / 30 + fx));
-                        circle(fx * 16 - 100, fy * 16 + 30, cosize, 191);
+                        circle(fx * 16 - 32 - camera.x / 2, fy * 16 + 30, cosize, 191);
                     }
                 }
             }
