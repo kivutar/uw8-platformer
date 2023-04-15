d81a1a3c7e58c11fac4d3a0bdc2dd0b6451106ea
Jean-André Santoni <jean.andre.santoni@gmail.com>
2023-04-15 14:58:52 +0200

    Try this

--- build.zig
+++ build.zig
@@ -22,21 +22,19 @@ pub fn build(b: *std.build.Builder) void {
     lib.stack_size = 8192;
     lib.step.dependOn(&assets.step);
 
-    if (lib.install_step) |install_step| {
-        const run_filter_exports = b.addSystemCommand(&[_][]const u8{ "uw8", "filter-exports", "zig-out/lib/cart.wasm", "zig-out/lib/cart-filtered.wasm" });
-        run_filter_exports.step.dependOn(&install_step.step);
-
-        const run_wasm_opt = b.addSystemCommand(&[_][]const u8{ "wasm-opt", "-Oz", "-o", "zig-out/cart.wasm", "zig-out/lib/cart-filtered.wasm" });
-        run_wasm_opt.step.dependOn(&run_filter_exports.step);
-
-        const run_uw8_pack = b.addSystemCommand(&[_][]const u8{ "uw8", "pack", "-l", "9", "zig-out/cart.wasm", "zig-out/cart.uw8" });
-        run_uw8_pack.step.dependOn(&run_wasm_opt.step);
-
-        const make_opt = b.step("make_opt", "make size optimized cart");
-        make_opt.dependOn(&run_uw8_pack.step);
-
-        b.default_step = make_opt;
-    }
-
     b.installArtifact(lib);
+
+    const run_filter_exports = b.addSystemCommand(&[_][]const u8{ "uw8", "filter-exports", "zig-out/lib/cart.wasm", "zig-out/lib/cart-filtered.wasm" });
+    run_filter_exports.step.dependOn(&install_step.step);
+
+    const run_wasm_opt = b.addSystemCommand(&[_][]const u8{ "wasm-opt", "-Oz", "-o", "zig-out/cart.wasm", "zig-out/lib/cart-filtered.wasm" });
+    run_wasm_opt.step.dependOn(&run_filter_exports.step);
+
+    const run_uw8_pack = b.addSystemCommand(&[_][]const u8{ "uw8", "pack", "-l", "9", "zig-out/cart.wasm", "zig-out/cart.uw8" });
+    run_uw8_pack.step.dependOn(&run_wasm_opt.step);
+
+    const make_opt = b.step("make_opt", "make size optimized cart");
+    make_opt.dependOn(&run_uw8_pack.step);
+
+    b.default_step = make_opt;
 }
